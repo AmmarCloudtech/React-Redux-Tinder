977b5f0d9a543ef2355e58b3f51696146ff83c0f
Ammar <ammar.cloudtech@gmail.com>
2017-06-09 02:01:17 +0500

    Action Creators lect 45

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
-export default class BookList extends Component {
+
+class BookList extends Component {
   renderList() {
-    return this.props,books.map((book) => {
+    return this.props.books.map((book) => {
       return (
-        <li key ={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key ={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       );
     });
   }
@@ -17,3 +26,21 @@ export default class BookList extends Component {
     )
   }
 }
+
+function mapStateToProps(state) {
+  // first argument is a state, Whatever is returned form here (object) will show up as a props
+  // inside of a book list
+  return {
+    books: state.books
+  };
+}
+// Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed to all of our reducers
+  // thats what bindActionCreators is doing here with dispatch. Dispatch receives all actions like a funnel and spits them to all reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+// Promote BookList from a component to a container - it needs to know about this new dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList); // connect takes a function i.e. (mapStateToProps) and a component i.e. (BookList) and produces a container
+                                                                      // connect is a function called from react-redux library to forge a connection between component and redux to produce a container, imported on the top.

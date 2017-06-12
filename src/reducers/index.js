4c3007ab0fbf1fa8b14ad959c80174714bf5ad12
Ammar <ammar.cloudtech@gmail.com>
2017-06-12 23:21:03 +0500

    end of Tinder project and Section 4

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
-import ActiveBook from './reducer_active_book'; 
+import ActiveBook from './reducer_active_book';
 
+// Any key to the object that we provide to the combineReducers ends up as the key on our GLOBAL STATE.
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
   activeBook: ActiveBook
 });
 
--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,4 +1,5 @@
 // State argument is not application state, only the state this reducer is responsible for.
+// reducers are only ever called when an action occurs(is dispatched by an app)
 
 export default function (state = null, action) { // In ES6  state = null means if state is undefined set it to null.
   switch(action.type) {
